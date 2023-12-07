e3577bf4cde2fc34faa59eb680cb21c4e65f4b3a
Milo R. Jørgensen <mrj@holion.dk>
2023-12-07 21:56:00 +0100

    part 2 done

--- part2/index.ts
+++ part2/index.ts
@@ -4,6 +4,8 @@ const input = fs.readFileSync("./input", "utf-8");
 
 const lines = input.split("\n");
 
+const alphanumericNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
 const getAllNumbers = (line: string): number[] => {
     const characters = line.split("");
 
@@ -16,6 +18,24 @@ const getAllNumbers = (line: string): number[] => {
     return nums;
 }
 
+const convertLineToRealNumbers = (line: string) => {
+    const an = alphanumericNumbers.filter(num => {
+        return line.includes(num);
+    });
+
+    const sorted = an.map(num => {
+        return { index: line.indexOf(num), num: num }
+    }).sort((a, b) => {
+        return a.index - b.index;
+    }).map(num => num.num);
+
+    for (const num of sorted) {
+        line = line.replace(num, (alphanumericNumbers.indexOf(num) + 1).toString());
+    }
+
+    return line;
+}
+
 const getFirstDigit = (allNums: number[]) => {
     return allNums[0];
 }
@@ -23,16 +43,18 @@ const getFirstDigit = (allNums: number[]) => {
 const getLastDigit = (allNums: number[]) => {
     return allNums[allNums.length - 1];
 }
-console.log(lines);
+const newLines = lines.map(line => convertLineToRealNumbers(line));
 
 let sum: number = 0;
-for (const line of lines) {
+for (const line of newLines) {
     const allNums = getAllNumbers(line);
 
     const first = getFirstDigit(allNums);
     const last = getLastDigit(allNums);
 
     const concatinated = `${first}${last}`;
+
+    console.log(line, concatinated);
 
     sum += parseInt(concatinated);
 }
