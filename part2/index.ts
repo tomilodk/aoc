80cdcb6ce16537408ce612b0f5a546563937840f
Milo R. Jørgensen <mrj@holion.dk>
2023-12-08 21:34:35 +0100

    Refactor number extraction logic and calculate sum

--- part2/index.ts
+++ part2/index.ts
@@ -6,25 +6,24 @@ const lines = input.split("\n");
 
 const alphanumericNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-
 type Digit = {
     index: number,
     num: number
 }
 
-const getAllNumbers = (line: string): Digit[] => {
+const getAllNumbers = (line: string, reversed: boolean = false): Digit[] => {
     const characters = line.split("");
 
     const nums = characters.filter((character) => {
         const parsed = parseInt(character);
         return parsed.toString() != "NaN";
 
-    }).map(character => { return { index: line.indexOf(character), num: parseInt(character) } });
+    }).map(character => { return { index: reversed ? line.lastIndexOf(character) : line.indexOf(character), num: parseInt(character) } });
 
     return nums;
 }
 
-const getFirstAlphanumericDigit = (line: string): Digit => {
+const getFirstAlphanumericDigit = (line: string): Digit | undefined => {
     const an = alphanumericNumbers.filter(num => {
         return line.includes(num);
     });
@@ -38,16 +37,16 @@ const getFirstAlphanumericDigit = (line: string): Digit => {
     return sorted[0];
 }
 
-const getLastAlphanumericDigit = (line: string): Digit => {
+const getLastAlphanumericDigit = (line: string): Digit | undefined => {
     const an = alphanumericNumbers.filter(num => {
         return line.includes(num);
     });
 
     const sorted = an.map(num => {
-        return { index: line.indexOf(num), num: alphanumericNumbers.indexOf(num) + 1 }
+        return { index: line.lastIndexOf(num), num: alphanumericNumbers.indexOf(num) + 1 }
     }).sort((a, b) => {
         return a.index - b.index;
-    })
+    });
 
     return sorted[sorted.length - 1];
 }
@@ -66,18 +65,45 @@ let sum: number = 0;
 for (const line of lines) {
 
     const allNums = getAllNumbers(line);
-
     const firstDigit = getFirstDigit(allNums);
-    const lastDigit = getLastDigit(allNums);
+
+    const allNumsReversed = getAllNumbers(line, true);
+    const lastDigit = getLastDigit(allNumsReversed);
+
     const firstAlphanumericDigit = getFirstAlphanumericDigit(line);
     const lastAlphanumericDigit = getLastAlphanumericDigit(line);
 
-    const first = firstDigit == undefined ? firstAlphanumericDigit.num : firstDigit.index < firstAlphanumericDigit.index ? firstDigit.num.toString() : firstAlphanumericDigit.num;
-    const last = lastDigit == undefined ? lastAlphanumericDigit.num : lastDigit.index > lastAlphanumericDigit.index ? lastDigit.num.toString() : lastAlphanumericDigit.num;
+
+    let first: string = "";
+    let last: string = "";
+
+    if (firstDigit != undefined && firstAlphanumericDigit != undefined) {
+        if (firstDigit.index < firstAlphanumericDigit.index) {
+            first = firstDigit.num.toString();
+        } else {
+            first = firstAlphanumericDigit.num.toString();
+        }
+    } else if (firstDigit != undefined) {
+        first = firstDigit.num.toString();
+    } else if (firstAlphanumericDigit != undefined) {
+        first = firstAlphanumericDigit.num.toString();
+    }
+
+    if (lastDigit != undefined && lastAlphanumericDigit != undefined) {
+        if (lastDigit.index > lastAlphanumericDigit.index) {
+            last = lastDigit.num.toString();
+        } else {
+            last = lastAlphanumericDigit.num.toString();
+        }
+    } else if (lastDigit != undefined) {
+        last = lastDigit.num.toString();
+    } else if (lastAlphanumericDigit != undefined) {
+        last = lastAlphanumericDigit.num.toString();
+    }
 
     const concatinated = `${first}${last}`;
 
     sum += parseInt(concatinated);
 }
 
-console.log(sum);
+console.log(sum, lines.length);
