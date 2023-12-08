752ab9f0052a2e5498694e0393132240246593d4
Milo R. Jørgensen <mrj@holion.dk>
2023-12-08 21:04:54 +0100

    Refactor code to improve readability and efficiency

--- part2/index.ts
+++ part2/index.ts
@@ -6,56 +6,76 @@ const lines = input.split("\n");
 
 const alphanumericNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-const getAllNumbers = (line: string): number[] => {
+
+type Digit = {
+    index: number,
+    num: number
+}
+
+const getAllNumbers = (line: string): Digit[] => {
     const characters = line.split("");
 
-    const nums = characters.filter(character => {
+    const nums = characters.filter((character) => {
         const parsed = parseInt(character);
         return parsed.toString() != "NaN";
 
-    }).map(character => parseInt(character));
+    }).map(character => { return { index: line.indexOf(character), num: parseInt(character) } });
 
     return nums;
 }
 
-const convertLineToRealNumbers = (line: string) => {
+const getFirstAlphanumericDigit = (line: string): Digit => {
     const an = alphanumericNumbers.filter(num => {
         return line.includes(num);
     });
 
     const sorted = an.map(num => {
-        return { index: line.indexOf(num), num: num }
+        return { index: line.indexOf(num), num: alphanumericNumbers.indexOf(num) + 1 }
     }).sort((a, b) => {
         return a.index - b.index;
-    }).map(num => num.num);
+    })
 
-    for (let i = 0; i < sorted.length; i++) {
-        const num = i == 0 ? sorted[i] : sorted[sorted.length - 1];
-        line = line.replace(num, (alphanumericNumbers.indexOf(num) + 1).toString());
-    }
-
-    return line;
+    return sorted[0];
 }
 
-const getFirstDigit = (allNums: number[]) => {
+const getLastAlphanumericDigit = (line: string): Digit => {
+    const an = alphanumericNumbers.filter(num => {
+        return line.includes(num);
+    });
+
+    const sorted = an.map(num => {
+        return { index: line.indexOf(num), num: alphanumericNumbers.indexOf(num) + 1 }
+    }).sort((a, b) => {
+        return a.index - b.index;
+    })
+
+    return sorted[sorted.length - 1];
+}
+
+const getFirstDigit = (allNums: Digit[]): Digit | undefined => {
+    if (allNums.length == 0) return undefined;
     return allNums[0];
 }
 
-const getLastDigit = (allNums: number[]) => {
+const getLastDigit = (allNums: Digit[]): Digit | undefined => {
+    if (allNums.length == 0) return undefined;
     return allNums[allNums.length - 1];
 }
-const newLines = lines.map(line => convertLineToRealNumbers(line));
 
 let sum: number = 0;
-for (const line of newLines) {
+for (const line of lines) {
+
     const allNums = getAllNumbers(line);
 
-    const first = getFirstDigit(allNums);
-    const last = getLastDigit(allNums);
+    const firstDigit = getFirstDigit(allNums);
+    const lastDigit = getLastDigit(allNums);
+    const firstAlphanumericDigit = getFirstAlphanumericDigit(line);
+    const lastAlphanumericDigit = getLastAlphanumericDigit(line);
+
+    const first = firstDigit == undefined ? firstAlphanumericDigit.num : firstDigit.index < firstAlphanumericDigit.index ? firstDigit.num.toString() : firstAlphanumericDigit.num;
+    const last = lastDigit == undefined ? lastAlphanumericDigit.num : lastDigit.index > lastAlphanumericDigit.index ? lastDigit.num.toString() : lastAlphanumericDigit.num;
 
     const concatinated = `${first}${last}`;
-
-    console.log(line, concatinated);
 
     sum += parseInt(concatinated);
 }
